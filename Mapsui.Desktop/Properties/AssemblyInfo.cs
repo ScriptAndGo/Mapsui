b1aafd95b2cf914b49a41e6a621aed1a45022b96
Skymirrh <skymirrh@gmail.com>
2018-07-06 09:33:14 +0200

    Bump to version 1.3.3 (internal NuGet package version: 1.3.3-custom)

--- Mapsui.Desktop/Properties/AssemblyInfo.cs
+++ Mapsui.Desktop/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
  
 
 
-[assembly: AssemblyVersion("1.3.2")]
-[assembly: AssemblyFileVersion("1.3.2")]
-[assembly: AssemblyInformationalVersion("1.3.2")]
+[assembly: AssemblyVersion("1.3.3")]
+[assembly: AssemblyFileVersion("1.3.3")]
+[assembly: AssemblyInformationalVersion("1.3.3")]
